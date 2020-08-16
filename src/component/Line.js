8f931852a5ffcd9d5cd5e23ddd30ea9fce58699f
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-08-16 11:56:13 +0100

    modelling the charts

--- src/component/Line.js
+++ src/component/Line.js
@@ -5,7 +5,7 @@ import EChart from './Echart'
 export default function Line({ config }) {
     let option = {
         title: {
-            text: '堆叠区域图'
+            text: 'DYNAMIC CHARTS'
         },
         tooltip: {
             trigger: 'axis',
@@ -16,14 +16,7 @@ export default function Line({ config }) {
                 }
             }
         },
-        // legend: {
-        //     data: config.data.map()
-        // },
-        // toolbox: {
-        //     feature: {
-        //         saveAsImage: {}
-        //     }
-        // },
+
         grid: {
             left: '3%',
             right: '4%',
@@ -34,7 +27,7 @@ export default function Line({ config }) {
             {
                 type: 'category',
                 boundaryGap: false,
-                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                data: config.data.map(row => row[config.xAxis])
             }
         ],
         yAxis: [
@@ -42,52 +35,17 @@ export default function Line({ config }) {
                 type: 'value'
             }
         ],
-        series: [
-            {
-                name: '邮件营销',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {},
-                data: [120, 132, 101, 134, 90, 230, 210]
-            },
-            {
-                name: '联盟广告',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {},
-                data: [220, 182, 191, 234, 290, 330, 310]
-            },
-            {
-                name: '视频广告',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {},
-                data: [150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-                name: '直接访问',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {},
-                data: [320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-                name: '搜索引擎',
-                type: 'line',
-                stack: '总量',
-                label: {
-                    normal: {
-                        show: true,
-                        position: 'top'
-                    }
-                },
-                areaStyle: {},
-                data: [820, 932, 901, 934, 1290, 1330, 1320]
-            }
-        ]
+        series: config.yAxes.map(key => ({
+            type: 'line',
+            areaStyle: {},
+            data: config.data.map(row => row[key])
+
+        }))
+
+
+
     };
-    return <>
-        {JSON.stringify(config)}
-        <EChart config={option} />
-    </>
+
+    return <EChart config={option} />
+
 }
