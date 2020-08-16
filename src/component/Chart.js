0ca0c5a5438068330c5a14179247f3c142247dc3
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-08-16 01:33:11 +0100

    connecting echarts with chart wrapper object

---
+++ src/component/Chart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Chart({ config, children }) {
+    const { height, width } = config
+
+    return (
+        <section style={{ height, width }}>
+            {children}
+
+        </section>
+    )
+
+}
