0830ead5e41fd3051c6153a9bc8cd65a3becae1e
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-02 13:19:02 +0100

    dev

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   require("./mock-endPoints/mocks");
 }
 
